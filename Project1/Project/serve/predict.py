64d8628d67bca1f5516116531d7c4a0e36bd696a
monki <watertreader@hotmail.com>
2021-09-19 23:34:42 +0800

    3rd commit

--- Project1/Project/serve/predict.py
+++ Project1/Project/serve/predict.py
@@ -70,8 +70,8 @@ def predict_fn(input_data, model):
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    data_X = None
-    data_len = None
+    processed = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, processed)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -82,11 +82,15 @@ def predict_fn(input_data, model):
     data = data.to(device)
 
     # Make sure to put the model into evaluation mode
-    model.eval()
+    model.eval()  #will notify all your layers that you are in eval mode, that way, 
+                  #batchnorm or dropout layers will work in eval mode instead of training mode.
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
+    with torch.no_grad():                 #impacts the autograd engine and deactivate it
+        output = model.forward(data)
+    
 
-    result = None
+    result = np.round(output.numpy())
 
     return result
