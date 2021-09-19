a06a868448cf159614870ec76efa582f48df1160
monki <watertreader@hotmail.com>
2021-09-19 09:58:04 +0800

    2nd commit

--- Project1/Project/train/train.py
+++ Project1/Project/train/train.py
@@ -69,7 +69,35 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     
     # TODO: Paste the train() method developed in the notebook here.
 
-    pass
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:         
+            batch_X, batch_y = batch
+            
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+            
+            # TODO: Complete this train method to train the model provided.
+            
+            ## zero the parameter gradients
+            optimizer.zero_grad()
+            
+            ## forward pass through the network
+            forward_pass = model.forward(batch_X)
+            #forward_pass = model(batch_X)
+            
+            # calculate the batch loss
+            loss = loss_fn(forward_pass, batch_y)
+            
+            # backpropagation --compute the backward pass.
+            loss.backward()
+            
+            #optimization
+            optimizer.step()
+            
+            total_loss += loss.data.item()
+    print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
 
 
 if __name__ == '__main__':
